bb3db61a0784882a25d56d626aa6d4084a178947
christopher Thronebury <Chris@christophers-MacBook.local>
2018-05-23 22:08:25 -0400

    Add comments

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ const app = express();
 
 app.use(express.json());
 
+
+// Create Courses Array
 const courses = [
   { id: 1, name: "Course 1" },
   { id: 2, name: "Course 2" },
   { id: 3, name: "Course 3" }
 ];
 
+// Home Page Route
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// View All Courses Route
 app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
 
+// Add New Course Route
 app.post("/api/courses", (req, res) => {
   const schema = {
     name: Joi.string()
@@ -40,11 +45,16 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
+// Get Single Course Route
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
   if (!course) res.status(404).send("That course was not found");
   res.send(course);
 });
 
+
+// Set port from env variable or default ot 3000
 const port = process.env.PORT || 3000;
+
+// Set up app listener
 app.listen(port, console.log(`Listening on port ${port}`));
