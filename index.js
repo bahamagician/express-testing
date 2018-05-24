c130aed8073ca53265539993cd57e70e02a5ac54
SC Graphics <scgraphics@SCs-iMac.local>
2018-05-24 13:39:51 -0400

    add some middleware

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Joi = require("joi");
+const logger = require("./logger");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(logger);
+
+app.use(function(req, res, next) {
+  console.log("Authenticating...");
+  next();
+});
 
 // Create Courses Array
 const courses = [
